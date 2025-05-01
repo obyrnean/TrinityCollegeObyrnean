cfb6426f38a4fd98de621fb4f3a8dbf59d540ca5
Ane O'Byrne <obyrnean@tcd.ie>
2025-05-01 12:49:50 +0200

    showglobals.c

---
+++ Year3/Semester2/Concurrent_and_Operating_Systems/Practical2/showglobals.c
@@ -0,0 +1,25 @@
+#include "globals.c"
+#include "actions.c"
+#include "threads.c"
+
+printf("gB = %d\n", gB);
+printf("gC = %d\n", gC);
+printf("gD = %d\n", gD);
+printf("gE = %d\n", gE);
+printf("gF = %d\n", gF);
+printf("gG = %d\n", gG);
+printf("gH = %d\n", gH);
+printf("gI = %d\n", gI);
+printf("gJ = %d\n", gJ);
+printf("gK = %d\n", gK);
+printf("gL = %d\n", gL);
+printf("gM = %d\n", gM);
+printf("gN = %d\n", gN);
+printf("gP = %d\n", gP);
+printf("gQ = %d\n", gQ);
+printf("gS = %d\n", gS);
+printf("gT = %d\n", gT);
+printf("gU = %d\n", gU);
+printf("gV = %d\n", gV);
+printf("gX = %d\n", gX);
+printf("gZ = %d\n", gZ);
