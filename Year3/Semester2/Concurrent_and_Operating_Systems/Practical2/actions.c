6b11d250c4e2a4bfc80ec7f1e406bf63e745c809
Ane O'Byrne <obyrnean@tcd.ie>
2025-05-01 12:48:42 +0200

    actions.c

---
+++ Year3/Semester2/Concurrent_and_Operating_Systems/Practical2/actions.c
@@ -0,0 +1,337 @@
+// Practical 2 Action-Functions
+//
+// DO NOT MODIFY
+
+#include "globals.c"
+#include "threads.c"
+
+// Practical 2 Action-Functions
+//
+// DO NOT MODIFY
+
+
+void ac6tion_11() {
+  int loc1;
+  loc1 = 4;
+  DISPLAY("Action 11 loaded %d\n",loc1);
+  gI = (gI + loc1);
+}
+
+void act5ion_12() {
+  int loc1;
+  loc1 = 4;
+  DISPLAY("Action 12 loaded %d\n",loc1);
+  gH = loc1;
+}
+
+void act1ion_13() {
+  int loc1;
+  loc1 = 2;
+  DISPLAY("Action 13 loaded %d\n",loc1);
+  gQ = loc1;
+}
+
+void actio5n_14() {
+  int loc1;
+  loc1 = 9;
+  DISPLAY("Action 14 loaded %d\n",loc1);
+  gG = loc1;
+}
+
+void act2ion_15() {
+  int loc1;
+  loc1 = 8;
+  DISPLAY("Action 15 loaded %d\n",loc1);
+  gQ = (gQ + loc1);
+}
+
+void actio7n_16() {
+  int loc1;
+  loc1 = 1;
+  DISPLAY("Action 16 loaded %d\n",loc1);
+  gK = (gK + loc1);
+  puff();
+  int loc2;
+  loc2 = 2;
+  DISPLAY("Action 16 loaded %d\n",loc2);
+  gF = (gF * loc2);
+}
+
+void ac6tion_17() {
+  int loc1;
+  loc1 = 0;
+  DISPLAY("Action 17 loaded %d\n",loc1);
+  gX = loc1;
+}
+
+void ac5tion_18() {
+  int loc1;
+  loc1 = 2;
+  DISPLAY("Action 18 loaded %d\n",loc1);
+  gX = (gX + loc1);
+}
+
+
+void acti3on_21() {
+  int loc1;
+  loc1 = 3;
+  DISPLAY("Action 21 loaded %d\n",loc1);
+  gK = (gK + loc1);
+  puff();
+  int loc2;
+  loc2 = 4;
+  DISPLAY("Action 21 loaded %d\n",loc2);
+  gF = (gF * loc2);
+}
+
+void act9ion_22() {
+  int loc1;
+  loc1 = gE;
+  DISPLAY("Action 22 loaded %d\n",loc1);
+  gZ = (gZ + (loc1 + 4));
+  int loc2;
+  loc2 = gS;
+  DISPLAY("Action 22 loaded %d\n",loc2);
+  gZ = (gZ + (loc2 + 3));
+  int loc3;
+  loc3 = gV;
+  DISPLAY("Action 22 loaded %d\n",loc3);
+  gZ = (gZ + (loc3 + 0));
+}
+
+void ac2tion_23() {
+  int loc1;
+  loc1 = 10;
+  DISPLAY("Action 23 loaded %d\n",loc1);
+  gG = (gG + loc1);
+}
+
+void acti1on_24() {
+  int loc1;
+  loc1 = 3;
+  DISPLAY("Action 24 loaded %d\n",loc1);
+  gN = loc1;
+}
+
+void acti9on_25() {
+  int loc1;
+  loc1 = 6;
+  DISPLAY("Action 25 loaded %d\n",loc1);
+  gG = loc1;
+}
+
+void actio7n_26() {
+  int loc1;
+  loc1 = 4;
+  DISPLAY("Action 26 loaded %d\n",loc1);
+  gI = (gI + loc1);
+}
+
+void ac3tion_27() {
+  int loc1;
+  loc1 = 2;
+  DISPLAY("Action 27 loaded %d\n",loc1);
+  gX = loc1;
+}
+
+void actio7n_28() {
+  int loc1;
+  loc1 = 2;
+  DISPLAY("Action 28 loaded %d\n",loc1);
+  gX = (gX - loc1);
+}
+
+
+void actio7n_31() {
+  int loc1;
+  loc1 = 9;
+  DISPLAY("Action 31 loaded %d\n",loc1);
+  gP = loc1;
+}
+
+void act8ion_32() {
+  int loc1;
+  loc1 = 2;
+  DISPLAY("Action 32 loaded %d\n",loc1);
+  gF = (gK * loc1);
+  puff();
+  int loc2;
+  loc2 = 10;
+  DISPLAY("Action 32 loaded %d\n",loc2);
+  gK = (gF + loc2);
+}
+
+void actio4n_33() {
+  int loc1;
+  loc1 = 8;
+  DISPLAY("Action 33 loaded %d\n",loc1);
+  gQ = (gQ + loc1);
+}
+
+void ac8tion_34() {
+  int loc1;
+  loc1 = gS;
+  DISPLAY("Action 34 loaded %d\n",loc1);
+  gT = (gT + (loc1 + 1));
+  int loc2;
+  loc2 = gE;
+  DISPLAY("Action 34 loaded %d\n",loc2);
+  gT = (gT + (loc2 - 3));
+}
+
+void acti8on_35() {
+  int loc1;
+  loc1 = 3;
+  DISPLAY("Action 35 loaded %d\n",loc1);
+  gB = loc1;
+}
+
+void actio7n_36() {
+  int loc1;
+  loc1 = 6;
+  DISPLAY("Action 36 loaded %d\n",loc1);
+  gM = (gM + loc1);
+}
+
+void act1ion_37() {
+  int loc1;
+  loc1 = 7;
+  DISPLAY("Action 37 loaded %d\n",loc1);
+  gD = loc1;
+}
+
+void actio8n_38() {
+  int loc1;
+  loc1 = 4;
+  DISPLAY("Action 38 loaded %d\n",loc1);
+  gD = (gD + loc1);
+}
+
+
+void act9ion_41() {
+  int loc1;
+  loc1 = 8;
+  DISPLAY("Action 41 loaded %d\n",loc1);
+  gQ = (gQ + loc1);
+}
+
+void act8ion_42() {
+  int loc1;
+  loc1 = gV;
+  DISPLAY("Action 42 loaded %d\n",loc1);
+  gC = (gC + (loc1 + 2));
+  int loc2;
+  loc2 = gS;
+  DISPLAY("Action 42 loaded %d\n",loc2);
+  gC = (gC + (loc2 - 9));
+}
+
+void act7ion_43() {
+  int loc1;
+  loc1 = 1;
+  DISPLAY("Action 43 loaded %d\n",loc1);
+  gJ = loc1;
+}
+
+void act3ion_44() {
+  int loc1;
+  loc1 = 5;
+  DISPLAY("Action 44 loaded %d\n",loc1);
+  gK = (gK + loc1);
+  puff();
+  int loc2;
+  loc2 = 6;
+  DISPLAY("Action 44 loaded %d\n",loc2);
+  gF = (gF * loc2);
+}
+
+void ac1tion_45() {
+  int loc1;
+  loc1 = 6;
+  DISPLAY("Action 45 loaded %d\n",loc1);
+  gM = (gM + loc1);
+}
+
+void acti4on_46() {
+  int loc1;
+  loc1 = 2;
+  DISPLAY("Action 46 loaded %d\n",loc1);
+  gB = loc1;
+}
+
+void ac4tion_47() {
+  int loc1;
+  loc1 = 4;
+  DISPLAY("Action 47 loaded %d\n",loc1);
+  gD = loc1;
+}
+
+void acti9on_48() {
+  int loc1;
+  loc1 = 3;
+  DISPLAY("Action 48 loaded %d\n",loc1);
+  gD = (gD + loc1);
+}
+
+
+void actio6n_51() {
+  int loc1;
+  loc1 = 10;
+  DISPLAY("Action 51 loaded %d\n",loc1);
+  gG = (gG + loc1);
+}
+
+void actio5n_52() {
+  int loc1;
+  loc1 = 10;
+  DISPLAY("Action 52 loaded %d\n",loc1);
+  gU = loc1;
+}
+
+void actio7n_53() {
+  int loc1;
+  loc1 = 6;
+  DISPLAY("Action 53 loaded %d\n",loc1);
+  gM = (gM + loc1);
+}
+
+void ac1tion_54() {
+  int loc1;
+  loc1 = gS;
+  DISPLAY("Action 54 loaded %d\n",loc1);
+  gM = (gM + (loc1 + 1));
+  int loc2;
+  loc2 = gQ;
+  DISPLAY("Action 54 loaded %d\n",loc2);
+  gM = (gM + (loc2 - 3));
+}
+
+void actio4n_55() {
+  int loc1;
+  loc1 = 1;
+  DISPLAY("Action 55 loaded %d\n",loc1);
+  gM = loc1;
+}
+
+void act8ion_56() {
+  int loc1;
+  loc1 = 4;
+  DISPLAY("Action 56 loaded %d\n",loc1);
+  gQ = loc1;
+}
+
+void acti6on_57() {
+  int loc1;
+  loc1 = 11;
+  DISPLAY("Action 57 loaded %d\n",loc1);
+  gD = loc1;
+}
+
+void ac9tion_58() {
+  int loc1;
+  loc1 = 7;
+  DISPLAY("Action 58 loaded %d\n",loc1);
+  gD = (gD - loc1);
+}
+
+
